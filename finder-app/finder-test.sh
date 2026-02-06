0783367cee11038ea241c8dc3dd326f30c18faaa
aditya <aditya@Ubuntu.myguest.virtualbox.org>
2026-02-06 14:04:54 -0700

    finder-app : finder_test.sh updated with the assignment 4 requirements
    Signed-off-by: aditya <aditya@Ubuntu.myguest.virtualbox.org>

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -8,7 +8,9 @@ set -u
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-username=$(cat conf/username.txt)
+
+USERNAME=$(cat /etc/finder-app/conf/username.txt)
+ASSIGNMENT=$(cat /etc/finder-app/conf/assignment.txt)
 
 if [ $# -lt 3 ]
 then
@@ -18,7 +20,7 @@ then
 		echo "Using default value ${NUMFILES} for number of files to write"
 	else
 		NUMFILES=$1
-	fi	
+	fi
 else
 	NUMFILES=$1
 	WRITESTR=$2
@@ -31,50 +33,34 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 
 rm -rf "${WRITEDIR}"
 
-# create $WRITEDIR if not assignment1
-assignment=`cat ../conf/assignment.txt`
-
-if [ $assignment != 'assignment1' ]
+if [ "$ASSIGNMENT" != "assignment1" ]
 then
 	mkdir -p "$WRITEDIR"
-
-	#The WRITEDIR is in quotes because if the directory path consists of spaces, then variable substitution will consider it as multiple argument.
-	#The quotes signify that the entire string in WRITEDIR is a single string.
-	#This issue can also be resolved by using double square brackets i.e [[ ]] instead of using quotes.
-	if [ -d "$WRITEDIR" ]
-	then
-		echo "$WRITEDIR created"
-	else
+	if [ ! -d "$WRITEDIR" ]; then
 		exit 1
 	fi
 fi
-#echo "Removing the old writer utility and compiling as a native application"
-#make clean
-#make
 
-echo "Compiling the writer application..."
-if [ ! -f ./writer ]; then
-    make clean
-    make
-fi
-
-
-for i in $( seq 1 $NUMFILES)
+# writer is expected to be in PATH (/usr/bin)
+for i in $(seq 1 $NUMFILES)
 do
-	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	writer "$WRITEDIR/${USERNAME}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+# finder.sh is expected to be in PATH (/usr/bin)
+OUTPUTSTRING=$(finder.sh "$WRITEDIR" "$WRITESTR")
 
-# remove temporary directories
+# Write output for assignment 4 requirement
+echo "$OUTPUTSTRING" > /tmp/assignment4-result.txt
+
+# Cleanup
 rm -rf /tmp/aeld-data
 
-set +e
-echo ${OUTPUTSTRING} | grep "${MATCHSTR}"
+echo "$OUTPUTSTRING" | grep "${MATCHSTR}" >/dev/null
 if [ $? -eq 0 ]; then
 	echo "success"
 	exit 0
 else
-	echo "failed: expected  ${MATCHSTR} in ${OUTPUTSTRING} but instead found"
+	echo "failed: expected ${MATCHSTR} in ${OUTPUTSTRING}"
 	exit 1
 fi

1333a64798b9adf96bfefc63b22e67068b0c0413
aditya <aditya@Ubuntu.myguest.virtualbox.org>
2026-03-08 20:23:05 -0600

    Final changes to the start stop script
    Signed-off-by: aditya <aditya@Ubuntu.myguest.virtualbox.org>

--- aesd-char-driver/aesdchar-start-stop.sh
+++ aesd-char-driver/aesdchar-start-stop.sh
@@ -3,11 +3,11 @@
 case "$1" in
     start)
         echo "Loading aesdchar"
-        /usr/bin/aesdchar_load
+        aesdchar_load
         ;;
     stop)
         echo "Unloading aesdchar"
-        /usr/bin/aesdchar_unload
+        aesdchar_unload
         ;;
         *)
         echo "Usage: $0 {start|stop}"
